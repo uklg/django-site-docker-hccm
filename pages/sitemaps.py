000d3579ca07946ed436deaaf957f090b64d1372
Richard Murdoch <rmu@live.co.uk>
2024-04-29 09:38:09 +0100

    fix sitemap lastmod

--- pages/sitemaps.py
+++ pages/sitemaps.py
@@ -1,11 +1,15 @@
 # sitemaps.py
 from django.contrib import sitemaps
 from django.urls import reverse
+from django.utils import timezone
 
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = 'daily'
     protocol = 'https'
+
+    def lastmod(self,item):
+        return timezone.now()
 
     def items(self):
         return ['demolition',]
